46b295eb93c9f792b86401663fc641e3fcadbd05
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-05-14 14:24:25 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,6 +49,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/foo', function () {
+    Artisan::call('storage:link');
 });
 
 require __DIR__.'/auth.php';
