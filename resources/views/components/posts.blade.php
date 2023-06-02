ef092ce7cc76ef597a5f9d3bbb76bcfb4dd850ed
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-06-02 12:10:38 +0300

    minor

--- resources/views/components/posts.blade.php
+++ resources/views/components/posts.blade.php
@@ -4,7 +4,9 @@
         <h2 class="title_block">{{$title}}</h2>
         <div class="preview_cards_wrap">
             @foreach($posts as $post)
-                <div onclick="window.open('{{route('post_page', $post['id'])}}','mywindow');" style="background-image: url('{{$post->getFirstMediaUrl('post_cover')}}')" class="preview_card">
+                <div data-post-id="{{$post['id']}}"
+                     onclick="window.open('{{route('post_page', $post['id'])}}','mywindow');"
+                     style="background-image: url('{{$post->getFirstMediaUrl('post_cover')}}')" class="post_wrap preview_card">
                     <h3 class="title">{{$post['title']}}</h3>
                     <a href="{{route('post_page', $post['id'])}}" class="link-bg black">
                         Читать
@@ -16,5 +18,38 @@
                 </div>
             @endforeach
         </div>
+        <a id="load_more_posts" class="link-bg orange">Загрузить еще</a>
+        <p class="all_loaded_text"></p>
+        @push('scripts')
+            <script>
+                cards_to_show = 3
+                cards_to_add = 3
+                card = $('.post_wrap')
+                cards_total = card.length
+
+                function show_cards(cards_to_show) {
+                    card.each(function(index) {
+                        if(index > cards_to_show - 1) {
+                            $(this).hide()
+                        } else {
+                            $(this).show()
+                        }
+                    })
+
+                    if(cards_to_show >= cards_total) {
+                        $('#load_more_posts').hide()
+
+                        text = 'Все статьи загружены (' + cards_total + ')'
+                        $('.all_loaded_text').text(text)
+                    }
+
+                }
+                show_cards(cards_to_show)
+                $('#load_more_posts').on('click', function() {
+                    cards_to_show += cards_to_add;
+                    show_cards(cards_to_show)
+                })
+            </script>
+        @endpush
     </div>
 </div>
