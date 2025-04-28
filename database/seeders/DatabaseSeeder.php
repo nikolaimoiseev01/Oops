f1b04798749d59b6667b7c80173d4a0b640cf9a6
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-04-29 00:36:05 +0300

    minor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,55 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+    public function make_products()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $good = \App\Models\Good::create([
+                'title' => 'Товар ' . $i,
+                'desc' => $faker->paragraph(),
+                'good_category_id' => 1,
+                'capacity' => $faker->randomFloat(2, 0, 100),
+                'capacity_type' => 'мл',
+                'compound' => 'Химический состав',
+                'properties' => [
+                    [
+                        'Тип свойства' => "Животное",
+                        "Значение" => "Кошка"
+                    ],
+                    [
+                        "Тип свойства" => "Вид загрязнения",
+                        "Значение" => "Запах"
+                    ]
+                ],
+                'advantages' => ["Безопасен для человека и животных", "Для любых поверхностей"],
+                'instruction' => $faker->paragraph(),
+                'links' => ["WB" => "vk.com", "Ozone"=> "vk.com"]
+            ]);
+
+            $good->addMediaFromUrl(url('/') . '/fixed/test/good_test_1.png')->preservingOriginal()->toMediaCollection('good_examples');
+            $good->addMediaFromUrl(url('/') . '/fixed/test/good_test_2.png')->preservingOriginal()->toMediaCollection('good_examples');
+            $good->addMediaFromUrl(url('/') . '/fixed/test/good_test_3.png')->preservingOriginal()->toMediaCollection('good_examples');
+        }
+    }
+
+    public function make_posts() {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $post = \App\Models\Post::create([
+                'title' => 'Пост ' . $i,
+                'content' => $faker->paragraph(),
+            ]);
+
+            $post->addMediaFromUrl(url('/') . '/fixed/test/good_test_1.png')->preservingOriginal()->toMediaCollection('post_cover');
+        }
+    }
+
     public function run(): void
     {
+        $this->make_products();
+        $this->make_posts();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
